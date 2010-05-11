c96923ccf48371bacd1decc8f1fd7d6b7f6303ff
Darshan Shaligram <scintilla@gmail.com>
2010-05-12 02:12:48 +0530

    Handle "partially mollified" milestones for Nemelex.

--- scload.py
+++ scload.py
@@ -347,7 +347,9 @@ def xlog_milestone_fixup(d):
   elif verb == 'god.renounce':
     noun = R_GOD_RENOUNCE.findall(milestone)[0]
   elif verb == 'god.mollify':
-    noun = R_GOD_MOLLIFY.findall(milestone)[0]
+    matches = R_GOD_MOLLIFY.findall(milestone)[0]
+    if matches:
+      noun = R_GOD_MOLLIFY.findall(milestone)[0]
   noun = noun or milestone
   d['verb'] = verb
   d['noun'] = noun
